7f93358b2a420b357a5a31004dd2fd1effa0b21e
D McFarland <dmcfarlandjr@gmail.com>
2012-05-11 23:00:24 -0700

    Added: New devise models for basic start-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 TutorFinder::Application.routes.draw do
 
+  devise_for :users,
+    :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register' }
+
   root :to => "home#index"
+
+  devise_scope :user do
+    get "login", :to => "devise/sessions#new"
+  end 
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
